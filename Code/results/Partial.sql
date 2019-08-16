bdcef0cd53fc9b8115a7edb90dd47e073797736a
Thomas James <tom.james@data-summit.co.uk>
2019-08-16 11:43:28 +0100

    Results database created

---
+++ Code/results/Partial.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Partial]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[resultSize] INT NULL,
+    [partialSize] INT NULL,
+    [partialStart] INT NULL,
+    [pageSize] INT NULL,
+    [sortByField] NVARCHAR(255) NOT NULL,
+    [ascending] BIT NOT NULL,
+    [query] NVARCHAR(255) NOT NULL,
+    [queryType] NVARCHAR(255) NOT NULL,
+    [seconds] DECIMAL(8,4) NOT NULL
+)
