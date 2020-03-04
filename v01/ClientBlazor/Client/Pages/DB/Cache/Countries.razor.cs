b05fbd2308c33756a339ff3cbc9c0f664025d505
Thomas James <tom.james@data-summit.co.uk>
2020-03-04 02:35:58 +0000

    Client side blazor tests

---
+++ v01/ClientBlazor/Client/Pages/DB/Cache/Countries.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Model.DB.Cache;
+using ClientBlazor.Shared.Cache;
+
+namespace ClientBlazor.Client.Pages.DB.Cache
+{
+    public class CountriesBase : ComponentBase
+    {
+        public async Task<List<Country>> listCountries()
+        {
+            List<Country> countries = new List<Country>();
+            CountryController cc = new CountryController(new cacheContext());
+            countries = await cc.GetCountries();
+            return countries;
+        }
+    }
+}
