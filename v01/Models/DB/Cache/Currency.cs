32aa16ca64861088eeeaacb725e9835d7bfefdb2
Thomas James <tom.james@data-summit.co.uk>
2020-03-04 02:39:22 +0000

    Amendements to "Cache" to include countries, currencies and historic foreign exchange rates

---
+++ v01/Models/DB/Cache/Currency.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Models.DB.Cache
+{
+    public partial class Currency
+    {
+        public Currency()
+        {
+            ExchangeRate = new HashSet<ExchangeRate>();
+        }
+
+        public short CurrencyId { get; set; }
+        public string Entity { get; set; }
+        public string Name { get; set; }
+        public string AlphabeticCode { get; set; }
+        public short NumericCode { get; set; }
+        public byte MinorUnit { get; set; }
+        public bool IsFundYesNo { get; set; }
+        public short CountryId { get; set; }
+
+        public virtual Country Country { get; set; }
+        public virtual ICollection<ExchangeRate> ExchangeRate { get; set; }
+    }
+}
--- v01/Models/DB/Cache/cacheContext.cs
+++ v01/Models/DB/Cache/cacheContext.cs
@@ -15,7 +15,9 @@ namespace Models.DB.Cache
         {
         }
 
-        public virtual DbSet<Placeholder> Placeholder { get; set; }
+        public virtual DbSet<Country> Country { get; set; }
+        public virtual DbSet<Currency> Currency { get; set; }
+        public virtual DbSet<ExchangeRate> ExchangeRate { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -28,6 +30,79 @@ namespace Models.DB.Cache
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Country>(entity =>
+            {
+                entity.Property(e => e.CountryId).ValueGeneratedNever();
+
+                entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Iso)
+                    .IsRequired()
+                    .HasColumnName("ISO")
+                    .HasMaxLength(3)
+                    .IsUnicode(false)
+                    .IsFixedLength();
+
+                entity.Property(e => e.Iso3)
+                    .HasColumnName("ISO3")
+                    .HasMaxLength(3)
+                    .IsUnicode(false)
+                    .IsFixedLength();
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(127);
+
+                entity.Property(e => e.Numcode).HasColumnName("numcode");
+
+                entity.Property(e => e.Phonecode).HasColumnName("phonecode");
+
+                entity.Property(e => e.SentenceCaseName)
+                    .IsRequired()
+                    .HasMaxLength(127);
+            });
+
+            modelBuilder.Entity<Currency>(entity =>
+            {
+                entity.HasIndex(e => new { e.Name, e.AlphabeticCode, e.NumericCode, e.MinorUnit })
+                    .HasName("IDX_CNAME");
+
+                entity.Property(e => e.AlphabeticCode)
+                    .IsRequired()
+                    .HasMaxLength(3)
+                    .IsUnicode(false)
+                    .IsFixedLength();
+
+                entity.Property(e => e.Entity)
+                    .IsRequired()
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(255);
+
+                entity.HasOne(d => d.Country)
+                    .WithMany(p => p.Currency)
+                    .HasForeignKey(d => d.CountryId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Currency_Country");
+            });
+
+            modelBuilder.Entity<ExchangeRate>(entity =>
+            {
+                entity.Property(e => e.ExchangeRateId).ValueGeneratedNever();
+
+                entity.Property(e => e.Date).HasColumnType("date");
+
+                entity.Property(e => e.Rate).HasColumnType("decimal(18, 6)");
+
+                entity.HasOne(d => d.Currency)
+                    .WithMany(p => p.ExchangeRate)
+                    .HasForeignKey(d => d.CurrencyId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ExchangeRate_Currency");
+            });
+
             OnModelCreatingPartial(modelBuilder);
         }
 
