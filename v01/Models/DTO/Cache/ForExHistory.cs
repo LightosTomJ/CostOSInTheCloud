652c22a9845b5bd90aa4eecede98e62df164bbbf
Thomas James <tom.james@data-summit.co.uk>
2020-03-05 00:42:46 +0000

    Historic foriegn exchange currency data combined from: countries, currencies and 1990 monthly historic data.

---
+++ v01/Models/DTO/Cache/ForExHistory.cs
@@ -0,0 +1,17 @@
+﻿using Models.DB.Cache;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.DTO.Cache
+{
+    public class ForExHistory
+    {
+        public Country country { get; set; }
+        public List<Currency> currencies { get; set; }
+        public List<List<ExchangeRate>> rates { get; set; }
+
+        public ForExHistory()
+        { }
+    }
+}
