318de7688828c54366af9f23fcc89b4f805cb32b
Thomas James <tom.james@data-summit.co.uk>
2020-03-09 11:39:05 +0000

    Partial indice test data

---
+++ v01/Databases/cache/dbo/Data/MaterialCategory.sql
@@ -0,0 +1,123 @@
+﻿USE [cache]
+GO
+
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (1, N'Porcelain', 3, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (2, N'Blocks', 5, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (3, N'Cast-In-Place', 5, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (4, N'Leather', 8, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (5, N'Lacquer', 9, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (6, N'Paint', 9, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (7, N'Carpet', 10, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (8, N'Clear', 12, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (9, N'Oil', 13, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (10, N'Petrol', 13, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (11, N'Diesel', 13, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (12, N'Ethene', 13, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (13, N'Brick', 14, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (14, N'Aluminum', 15, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (15, N'Brass', 15, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (16, N'Bronze', 15, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (17, N'Platinium', 15, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (18, N'Checker Plate', 15, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (19, N'Chrome', 15, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (20, N'Steel', 15, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (21, N'Copper', 15, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (22, N'Gold', 15, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (23, N'Iron', 15, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (24, N'Lead', 15, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (25, N'Silver', 15, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (26, N'Steel Alloy', 15, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (27, N'Titanium', 15, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (28, N'Zinc', 15, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (29, N'Composite', 16, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (30, N'Insulation', 16, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (31, N'Rubber', 16, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (32, N'Polyethylene', 18, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (33, N'Nylon', 18, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (34, N'Shingles', 19, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (35, N'Slate', 19, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (36, N'Grass', 20, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (37, N'Gravel', 20, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (38, N'Sand', 20, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (39, N'Alabaster', 22, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (40, N'Granite', 22, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (41, N'Marble', 22, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (42, N'Riverstone', 22, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (43, N'Terrazzo', 22, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (44, N'Travertine', 22, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (45, N'Coarse', 23, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (46, N'Birch', 26, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (47, N'Cherry', 26, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (48, N'Elm', 26, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (49, N'Mahogany', 26, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (50, N'Maple', 26, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (51, N'Poplar', 26, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (52, N'Red Oak', 26, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (53, N'Redwood', 26, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (54, N'Plywood', 26, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (55, N'Rosewood', 26, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (56, N'Spruce', 26, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (57, N'Teak', 26, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (58, N'Walnut', 26, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (59, N'White Oak', 26, GETDATE())
+GO
+INSERT [dbo].[MaterialCategory] ([MaterialCategoryId], [Name], [MaterialGenreId], [CreatedDate]) VALUES (60, N'Pine', 26, GETDATE())
+GO
