23673122132566af70f903c4125d836eded9aab6
Thomas James <tom.james@data-summit.co.uk>
2020-03-26 20:49:52 +0000

    Config controllers created for; user, companies and associated information

---
+++ v01/API/Controllers/DB/Config/CompaniesController.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Models.DB.Config;
+
+namespace API.Controllers.DB.Config
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        private readonly configContext _context;
+
+        public CompaniesController(configContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Companies
+        [HttpGet]
+        public async Task<IEnumerable<Companies>> GetCompanies()
+        {
+            return await _context.Companies.ToListAsync();
+        }
+
+        // GET: api/Companies/5
+        [HttpGet("{id}")]
+        public async Task<Companies> GetCompanyById(int id)
+        {
+            var companies = await _context.Companies.FindAsync(id);
+
+            if (companies == null) return null;
+
+            return companies;
+        }
+
+        // PUT: api/Companies/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCompanies(int id, Companies companies)
+        {
+            if (id != companies.CompanyId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(companies).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CompaniesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Companies
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Companies>> PostCompanies(Companies companies)
+        {
+            _context.Companies.Add(companies);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCompanies", new { id = companies.CompanyId }, companies);
+        }
+
+        // DELETE: api/Companies/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Companies>> DeleteCompanies(int id)
+        {
+            var companies = await _context.Companies.FindAsync(id);
+            if (companies == null)
+            {
+                return NotFound();
+            }
+
+            _context.Companies.Remove(companies);
+            await _context.SaveChangesAsync();
+
+            return companies;
+        }
+
+        private bool CompaniesExists(int id)
+        {
+            return _context.Companies.Any(e => e.CompanyId == id);
+        }
+    }
+}
---
+++ v01/API/Controllers/DB/Config/GendersController.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Models.DB.Config;
+
+namespace API.Controllers.DB.Config
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GendersController : ControllerBase
+    {
+        private readonly configContext _context;
+
+        public GendersController(configContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Genders
+        [HttpGet]
+        public async Task<List<Genders>> GetGenders()
+        {
+            return await _context.Genders.ToListAsync();
+        }
+
+        // GET: api/Genders/5
+        [HttpGet("{id}")]
+        public async Task<Genders> GetGender(byte id)
+        {
+            var genders = await _context.Genders.FindAsync(id);
+
+            return genders;
+        }
+
+        // PUT: api/Genders/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGenders(byte id, Genders genders)
+        {
+            if (id != genders.GenderId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(genders).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GendersExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Genders
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Genders>> PostGenders(Genders genders)
+        {
+            _context.Genders.Add(genders);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (GendersExists(genders.GenderId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetGenders", new { id = genders.GenderId }, genders);
+        }
+
+        // DELETE: api/Genders/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Genders>> DeleteGenders(byte id)
+        {
+            var genders = await _context.Genders.FindAsync(id);
+            if (genders == null)
+            {
+                return NotFound();
+            }
+
+            _context.Genders.Remove(genders);
+            await _context.SaveChangesAsync();
+
+            return genders;
+        }
+
+        private bool GendersExists(byte id)
+        {
+            return _context.Genders.Any(e => e.GenderId == id);
+        }
+    }
+}
