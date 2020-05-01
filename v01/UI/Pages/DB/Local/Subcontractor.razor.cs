1af4789d78964e0d870ec3955211689ca050f46e
Thomas James <tom.james@data-summit.co.uk>
2020-05-01 13:53:45 +0100

    Consumables, subcontractors and suppliers
    Consumables, subcontractors and suppliers index views created

--- v01/UI/Pages/DB/Local/Subcontractor.razor.cs
+++ v01/UI/Pages/DB/Local/Subcontractor.razor.cs
@@ -1,22 +1,30 @@
-using Diagnostics.Logger; using Models.DB.Local;
+using Diagnostics.Logger;
+using Models.DB.Local;
+using Helper.DB.Local;
 using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Radzen;
+using System.Globalization;
 
 namespace UI.Pages.DB.Local
 {
     public class SubcontractorBase : ComponentBase
     {
-        protected IList<Models.DB.Local.Subcontractor> subcontractor = null;
+        protected IList<Models.DB.Local.Subcontractor> subcontractors = null;
+        protected static LocalContext localContext = new LocalContext();
+        protected SubcontractorsService subcontractorsService = new SubcontractorsService(localContext);
+        protected CurrenciesService currencyService = new CurrenciesService(localContext);
+        protected NumberFormatInfo nfi = new CultureInfo("en-GB", false).NumberFormat;
 
         protected override async Task OnInitializedAsync()
         {
             try
             {
-                //subcontractor = await GetSubcontractorAsync();
+                subcontractors = await subcontractorsService.GetAllSubcontractors();
+                nfi.CurrencyPositivePattern = 2;
             }
             catch (Exception ae)
             {
--- v01/UI/Pages/DB/Local/Supplier.razor.cs
+++ v01/UI/Pages/DB/Local/Supplier.razor.cs
@@ -1,22 +1,30 @@
-using Diagnostics.Logger; using Models.DB.Local;
+using Diagnostics.Logger;
+using Models.DB.Local;
+using Helper.DB.Local;
 using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Radzen;
+using System.Globalization;
 
 namespace UI.Pages.DB.Local
 {
     public class SupplierBase : ComponentBase
     {
-        protected IList<Models.DB.Local.Supplier> supplier = null;
+        protected IList<Models.DB.Local.Supplier> suppliers = null;
+        protected static LocalContext localContext = new LocalContext();
+        protected SuppliersService suppliersService = new SuppliersService(localContext);
+        protected CurrenciesService currencyService = new CurrenciesService(localContext);
+        protected NumberFormatInfo nfi = new CultureInfo("en-GB", false).NumberFormat;
 
         protected override async Task OnInitializedAsync()
         {
             try
             {
-                //supplier = await GetSupplierAsync();
+                suppliers = await suppliersService.GetAllSuppliers();
+                nfi.CurrencyPositivePattern = 2;
             }
             catch (Exception ae)
             {
